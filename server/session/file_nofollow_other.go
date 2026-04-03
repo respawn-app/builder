ec29172dae5d0995832a62cd057f69510805de43
Nek-12 <vaizin.nikita@gmail.com>
2026-04-03 16:04:31 +0200

    fix: split nofollow session file opens by platform

---
+++ server/session/file_nofollow_other.go
@@ -0,0 +1,20 @@
+//go:build !(darwin || dragonfly || freebsd || linux || netbsd || openbsd || solaris)
+
+package session
+
+import "os"
+
+func openSessionFileReadOnly(path string) (*os.File, error) {
+	info, err := os.Lstat(path)
+	if err != nil {
+		return nil, err
+	}
+	if info.Mode()&os.ModeSymlink != 0 {
+		return nil, errSessionFileSymlink
+	}
+	return os.Open(path)
+}
+
+func isSymlinkOpenError(err error) bool {
+	return err == errSessionFileSymlink
+}
