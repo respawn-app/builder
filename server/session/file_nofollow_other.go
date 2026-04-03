708b19224a3fa2edd577e6bbd8e9fb85c5448b43
Nek-12 <vaizin.nikita@gmail.com>
2026-04-03 16:22:19 +0200

    fix: harden session file opens on windows

--- server/session/file_nofollow_other.go
+++ server/session/file_nofollow_other.go
@@ -1,4 +1,4 @@
-//go:build !(darwin || dragonfly || freebsd || linux || netbsd || openbsd || solaris)
+//go:build !(darwin || dragonfly || freebsd || linux || netbsd || openbsd || solaris || windows)
 
 package session
 
