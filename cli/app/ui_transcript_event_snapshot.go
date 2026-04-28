ef75f265b0c3cfd4de163cab114b1ad0c1e27cba
Builder Test <builder@test.invalid>
2026-04-28 15:01:51 +0200

    refactor: extract deferred transcript tail reducer

---
+++ cli/app/ui_transcript_event_snapshot.go
@@ -0,0 +1,30 @@
+package app
+
+import "strings"
+
+func projectedTranscriptEventSnapshotFromModel(m *uiModel) projectedTranscriptEventSnapshot {
+	if m == nil {
+		return projectedTranscriptEventSnapshot{}
+	}
+	return projectedTranscriptEventSnapshot{
+		entries:              m.transcriptEntries,
+		baseOffset:           m.transcriptBaseOffset,
+		revision:             m.transcriptRevision,
+		hasRuntimeClient:     m.hasRuntimeClient(),
+		busy:                 m.busy,
+		liveAssistantPending: strings.TrimSpace(m.view.OngoingStreamingText()) != "" || m.sawAssistantDelta,
+	}
+}
+
+func deferredCommittedTailSnapshotFromModel(m *uiModel) deferredCommittedTailSnapshot {
+	if m == nil {
+		return deferredCommittedTailSnapshot{}
+	}
+	return deferredCommittedTailSnapshot{
+		tails:            m.deferredCommittedTail,
+		committedEntries: committedTranscriptEntriesForApp(m.transcriptEntries),
+		baseOffset:       m.transcriptBaseOffset,
+		revision:         m.transcriptRevision,
+		totalEntries:     m.transcriptTotalEntries,
+	}
+}
